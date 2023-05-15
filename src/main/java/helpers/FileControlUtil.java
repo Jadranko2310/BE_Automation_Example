87716a3a71ccc7fdea7d35302ca011185cca25f0
Jadranko2310 <jadrankojankovicr@gmail.com>
2023-05-15 16:10:34 +0200

    adding maven profile in POM so that env can be change as Maven Propertie

--- src/main/java/helpers/FileControlUtil.java
+++ src/main/java/helpers/FileControlUtil.java
@@ -9,16 +9,14 @@ import java.util.Properties;
 public class FileControlUtil {
   public Properties properties;
 
-  public static final String ENDPOINTS_DATA_PROPERTIES = "Endpoints.properties";
+  public static final String ENDPOINTS_DATA_PROPERTIES = "base_URL.properties";
   public static final String USER_DATA_PROPERTIES = "UserDetails.properties";
-  public static final String RESPONSE_MESSAGE = "ResponseMessage.properties";
 
 
   public FileControlUtil(String file) throws Exception {
     properties = new Properties();
     properties.load(getClass().getResourceAsStream("/" + file));
   }
-
 
   public String getValue(String propertyKey) {
     return  properties.getProperty(propertyKey);
